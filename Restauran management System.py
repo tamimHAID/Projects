632a92bb72d6a65edbc1743261a2f0cb8ab27917
tamimHAID <158749345+tamimHAID@users.noreply.github.com>
2024-10-30 19:52:30 +0600

    Add files via upload

---
+++ Restauran management System.py
@@ -0,0 +1,27 @@
+menu = {
+    "Coffee": 200,
+    "Burger": 300,
+    "Chicken": 500,
+    "Ice cream": 150,
+    "Sweets": 350
+}
+
+print("Welcome to my Restaurant! Here is the menu:")
+for item, price in menu.items():
+    print(f"{item} : {price} BDT")
+
+total_price = 0
+
+while True:
+    item = input("\nWhat would you like to order, Sir? ").capitalize()
+    if item in menu:
+        total_price += menu[item]
+        print(f"Added {item} to your order. Current total: {total_price} BDT.")
+    else:
+        print("Sorry, that item is not available.")
+    
+    another_order = input("Would you like to order something else? (yes/no) ").strip().lower()
+    if another_order != "yes":
+        break
+
+print(f"\nYour final total is {total_price} BDT. Thank you for dining with us!")
